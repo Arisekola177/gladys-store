0177223be6d69b0c42cdd0c4a0be9310cd94511d
Arisekola <shuaibazeez14@gmail.com>
2024-03-31 17:13:16 +0100

    firebase rectified

--- firebase.config.js
+++ firebase.config.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.MESSAGINGSENDERID,
-  appId: process.env.APPID
+  apiKey: "AIzaSyDI2Hsm0SPh4uamiVRu0_4NdU4EwP-3HGo",
+  authDomain: "gladys-real.firebaseapp.com",
+  projectId: "gladys-real",
+  storageBucket: "gladys-real.appspot.com",
+  messagingSenderId: "478948736614",
+  appId: "1:478948736614:web:8b73d680bbb07d1e5ef7a7"
 };
 
 export const app = initializeApp(firebaseConfig);
